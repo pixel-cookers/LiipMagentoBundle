6674dec9e9c79845b798d03e1bde8badba8a0a2f
jaugustin <jeremie.augustin@pixel-cookers.com>
2012-06-06 19:15:33 +0200

    sf 2.1 security factory loading

--- LiipMagentoBundle.php
+++ LiipMagentoBundle.php
@@ -3,15 +3,19 @@
 namespace Liip\MagentoBundle;
 
 use Liip\MagentoBundle\DependencyInjection\Compiler\MagentoCompilerPass;
+use Liip\MagentoBundle\Security\MagentoAuthenticationFactory;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class LiipMagentoBundle extends Bundle
-{   
-   public function build(ContainerBuilder $container) 
+{
+   public function build(ContainerBuilder $container)
    {
-       parent::build($container);              
+       parent::build($container);
        $container->addCompilerPass(new MagentoCompilerPass());
+
+       $extension = $container->getExtension('security');
+       $extension->addSecurityListenerFactory(new MagentoAuthenticationFactory());
 
    }
 }
